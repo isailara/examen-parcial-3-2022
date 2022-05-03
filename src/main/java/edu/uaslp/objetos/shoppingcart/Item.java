1192c4c72dd46d78f3254fb6876bee60169464f5
isailara2 <isailara20002@gmail.com>
2022-05-03 08:51:34 -0500

    Parcial 3 - Examen

--- src/main/java/edu/uaslp/objetos/shoppingcart/Item.java
+++ src/main/java/edu/uaslp/objetos/shoppingcart/Item.java
@@ -11,11 +11,11 @@ public class Item {
     public Item(String itemCode, String providerCode, BigDecimal unitCost, int quantity) {
         if ((itemCode == null) || (itemCode == "")){
             throw new InvalidDataException("Null or empty string not allowed for item code");
-        }if (providerCode == null || providerCode == ""){
+        }if ((providerCode == null) || (providerCode == "")){
             throw new InvalidDataException("Null or empty string not allowed for provider code");
         }if (unitCost.compareTo(new BigDecimal("0")) <= 0){
             throw new InvalidDataException("Cost must be greater than zero");
-        }if (quantity < 0 || quantity > 5) {
+        }if ((quantity < 0) || (quantity > 5)) {
             throw new InvalidDataException("Quantity must be greater than zero and lower than 5");
         }
         this.itemCode=itemCode;
