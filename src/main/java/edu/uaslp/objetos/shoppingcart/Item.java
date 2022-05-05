b1384f70e8dc9ec9b5ef10c60ab2a604d97273a4
isailara2 <isailara20002@gmail.com>
2022-05-05 17:55:45 -0500

    Corrección - Parcial 3 - Examen

--- src/main/java/edu/uaslp/objetos/shoppingcart/Item.java
+++ src/main/java/edu/uaslp/objetos/shoppingcart/Item.java
@@ -3,21 +3,13 @@ package edu.uaslp.objetos.shoppingcart;
 import java.math.BigDecimal;
 
 public class Item {
-    String itemCode;
-    String providerCode;
-    int quantity;
-    BigDecimal unitCost;
+    private String itemCode;
+    private String providerCode;
+    private int quantity;
+    private BigDecimal unitCost;
 
     public Item(String itemCode, String providerCode, BigDecimal unitCost, int quantity) {
-        if ((itemCode == null) || (itemCode == "")){
-            throw new InvalidDataException("Null or empty string not allowed for item code");
-        }if ((providerCode == null) || (providerCode == "")){
-            throw new InvalidDataException("Null or empty string not allowed for provider code");
-        }if (unitCost.compareTo(new BigDecimal("0")) <= 0){
-            throw new InvalidDataException("Cost must be greater than zero");
-        }if ((quantity < 0) || (quantity > 5)) {
-            throw new InvalidDataException("Quantity must be greater than zero and lower than 5");
-        }
+
         this.itemCode=itemCode;
         this.providerCode=providerCode;
         this.unitCost=unitCost;
--- src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
+++ src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
@@ -3,37 +3,63 @@ package edu.uaslp.objetos.shoppingcart;
 import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
-
 public class ShoppingCart {
-    List<Item> itemsList = new LinkedList<>();
-    BigDecimal totalCost= BigDecimal.ZERO;
-    int itemsCount;
+
+    private List<Item> itemsList = new LinkedList<>();
+    private int itemsCount;
     public boolean isEmpty() {
         return itemsList.size() == 0;
     }
+    public BigDecimal getTotalCost(){
+        if(itemsList.isEmpty()){
+            throw new EmptyShoppingCartException();
+        }
+        BigDecimal totalCost = BigDecimal.ZERO;
 
-    public BigDecimal getTotalCost() {
         for(Item item: itemsList){
             totalCost = totalCost.add(item.getUnitCost().multiply(BigDecimal.valueOf(item.getQuantity())));
         }
         return totalCost;
     }
-
     public void addItem(Item item){
-        itemsList.add(item);
-        itemsCount++;
-    }
 
+        if ((item.getCode() == null) || (item.getCode() == "")){
+            throw new InvalidDataException("Null or empty string not allowed for item code");
+        }if ((item.getProviderCode() == null) || (item.getProviderCode() == "")){
+            throw new InvalidDataException("Null or empty string not allowed for provider code");
+        }if (item.getUnitCost().compareTo(BigDecimal.ZERO) < 0){
+            throw new InvalidDataException("Cost must be greater than zero");
+        }if ((item.getQuantity() < 1) || (item.getQuantity() > 5)) {
+            throw new InvalidDataException("Quantity must be greater than zero and lower than 5");
+        }
+
+        for(Item addItem: itemsList){
+            if(addItem.getCode() == item.getCode() && addItem.getUnitCost().compareTo(item.getUnitCost())==0){
+                addItem.setQuantity(addItem.getQuantity()+item.getQuantity());
+                return;
+            }
+        }
+        itemsList.add(item);
+        //itemsCount++;
+    }
     public int getItemsCount() {
-        //itemsCount = itemsList.size();
+
+        for (Item item : itemsList) {
+            itemsCount = itemsCount + item.getQuantity();
+        }
         return itemsCount;
     }
-
     public List<Item> getItems() {
         return itemsList;
     }
-
     public void removeItem(String itemCode) {
-        itemsList.remove(itemCode);
+        for(Item removeItem: itemsList){
+            if(removeItem.getCode()==itemCode){
+                removeItem.setQuantity(removeItem.getQuantity()-1);
+                if(removeItem.getQuantity()==0)
+                    itemsList.remove(removeItem);
+                return;
+            }
+        }
     }
 }
